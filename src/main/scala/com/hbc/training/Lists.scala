7f359eed144769a457e541cbc740de6791aecd42
dfung-s5a <dennis.fung@hbc.com>
2019-03-14 11:37:03 -0400

    created fold

--- src/main/scala/com/hbc/training/Lists.scala
+++ src/main/scala/com/hbc/training/Lists.scala
@@ -12,19 +12,37 @@ private[training] trait Lists {
 
   final def mapListR[A, B]: (A => B) => ListR[A] => ListR[B] = f => list => reverseR[B](mapListR(list, nil[B], f))
 
+  final def reverseR[A]: ListR[A] => ListR[A] = reverseR(_, nil)
+
+//  final def sumR: ListR[Int] => Int = sumR(_, 0)
+
+  final def fold[A,B]: (B => A => B) => B => ListR[A] => B = acc => zero => foldLoop(_, zero, acc)
+
+  final def sumR: ListR[Int] => Int = fold[Int,Int](b => a => a + b)(0)
+
+  @tailrec
+  private def foldLoop[A,B](list: ListR[A], acc: B, f: B => A => B): B = list match {
+    case Nil => acc
+    case Cons(h,t) => foldLoop(t, f(acc)(h), f)
+  }
+
   @tailrec
   private def mapListR[A,B](list:ListR[A], acc:ListR[B], f:A => B): ListR[B] = list match {
     case Nil => acc
     case Cons(h, t) => mapListR(t, cons(f(h), acc), f)
   }
 
-  final def reverseR[A]: ListR[A] => ListR[A] = reverseR(_, nil)
-
   @tailrec
   private def reverseR[A](list: ListR[A], acc: ListR[A]): ListR[A] = list match {
     case Nil => acc
     case Cons(h,t) => reverseR(t, cons(h, acc))
   }
+
+//  @tailrec
+//  private def sumR(list: ListR[Int], acc: Int): Int = list match {
+//    case Nil => acc
+//    case Cons(h,t) => sumR(t, acc + h)
+//  }
 
   def headR[A]: ListR[A] => Maybe[A] = {
     case Cons(head, _) => just(head)
@@ -44,13 +62,5 @@ private[training] trait Lists {
   def tailL[A]: ListL[A] => Maybe[A] = {
     case Snoc(_, tail) => just(tail)
     case Lin => empty
-  }
-
-  final def sumR: ListR[Int] => Int = sumR(_, 0)
-
-  @tailrec
-  private def sumR(list: ListR[Int], acc: Int): Int = list match {
-    case Nil => acc
-    case Cons(h,t) => sumR(t, acc + h)
   }
 }
