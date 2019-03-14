f4605fc2adf3175dd97c7ac1f43d7cdec7bb7ef1
dfung-s5a <dennis.fung@hbc.com>
2019-03-14 12:00:25 -0400

    finality

--- src/main/scala/com/hbc/training/Lists.scala
+++ src/main/scala/com/hbc/training/Lists.scala
@@ -5,10 +5,10 @@ import scala.annotation.tailrec
 //private[training] only available in training
 private[training] trait Lists {
 
-  def cons[A](head:A, tail:ListR[A]):ListR[A] = Cons(head, tail)
-  def nil[A]:ListR[A] = Nil
-  def snoc[A](head:ListL[A], tail:A):ListL[A] = Snoc(head, tail)
-  def lin[A]:ListL[A] = Lin
+  final def cons[A](head:A, tail:ListR[A]):ListR[A] = Cons(head, tail)
+  final def nil[A]:ListR[A] = Nil
+  final def snoc[A](head:ListL[A], tail:A):ListL[A] = Snoc(head, tail)
+  final def lin[A]:ListL[A] = Lin
 
   final def mapListR[A,B]: (A => B) => ListR[A] => ListR[B] =
     f => as => reverseR(fold[A,ListR[B]](bs => a => cons(f(a), bs))(nil[B])(as))
@@ -25,22 +25,22 @@ private[training] trait Lists {
     case Cons(h,t) => foldLoop(t, f(acc)(h), f)
   }
 
-  def headR[A]: ListR[A] => Maybe[A] = {
+  final def headR[A]: ListR[A] => Maybe[A] = {
     case Cons(head, _) => just(head)
     case Nil => empty
   }
 
-  def tailR[A]: ListR[A] => Maybe[ListR[A]] = {
+  final def tailR[A]: ListR[A] => Maybe[ListR[A]] = {
     case Cons(_, tail) => just(tail)
     case Nil => empty
   }
 
-  def headL[A]: ListL[A] => Maybe[ListL[A]] = {
+  final def headL[A]: ListL[A] => Maybe[ListL[A]] = {
     case Snoc(head, _) => just(head)
     case Lin => empty
   }
 
-  def tailL[A]: ListL[A] => Maybe[A] = {
+  final def tailL[A]: ListL[A] => Maybe[A] = {
     case Snoc(_, tail) => just(tail)
     case Lin => empty
   }
